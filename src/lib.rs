22099c43067888ae032aac0001c35aef2cf6c2b9
bjorn baron <bjornbaron@outlook.com>
2016-03-12 16:47:21 +0100

    Fix library function read_castle

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,12 @@ pub mod data;
 pub mod gbd;
 pub mod connection;
 
-pub fn read_castles(data_mgr: &mut DataMgr, user: &str, data: gbd::Gbd){
+pub fn read_castles(data_mgr: &mut data::DataMgr, user: &str, data: gbd::Gbd){
     let pid = data.gcl.find("PID").unwrap().as_u64().unwrap();
     let owner_name = user;
     let dcl = data.gcl.find("C").unwrap().as_array().unwrap();
     for world in dcl{
-        let world_name = World::from_int(world.find("KID").unwrap().as_u64().unwrap());
+        let world_name = data::World::from_int(world.find("KID").unwrap().as_u64().unwrap());
         let castles = world.find("AI").expect("No world AI").as_array().expect("AI not a array");
         for castle in castles{
             if castle.find("AI") == None{
@@ -20,7 +20,7 @@ pub fn read_castles(data_mgr: &mut DataMgr, user: &str, data: gbd::Gbd){
                 continue;
             }
             let castle = castle.find("AI").expect("No castle AI").as_array().expect("AI not a array");
-            let castle = Castle{
+            let castle = data::Castle{
                 id: castle[3].as_u64().unwrap(),
                 owner_id: Some(pid),
                 owner_name: Some(owner_name.to_string()),
